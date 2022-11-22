d2126ab400512662df6250245d7c933d974c1ef0
CelsoTC <ctdocabo@wpi.edu>
2022-11-22 11:09:59 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Project 4 Team 4
 Project 4 for reinforcement learning
 
+## Environment information
+
+The package `gym-2048` does not work with newer versions of Python. For now I have installed `python==3.6` in a new environment (I know version 3.10 does not work since it requires numpy 1.14)
+
+## Files
+
+- `main.py` has an example from gym-2048 on how to run the environment
+
 ## Important links
 
 https://pypi.org/project/gym-2048/
--- main.py
+++ main.py
@@ -1,51 +1,17 @@
 import gym_2048
 import gym
-from action import epsilon_greedy
-from collections import defaultdict
-import numpy as np
-
 
 if __name__ == '__main__':
   env = gym.make('2048-v0')
   env.seed(42)
 
-  observation = env.reset()
+  env.reset()
   env.render()
-  returns_sum = defaultdict(float)
-  returns_count = defaultdict(float)
-  epsilon = 1
+
   done = False
   moves = 0
-  gamma = 1
-  Q = defaultdict(lambda: np.zeros(env.action_space.n))
-  nA = env.action_space.n
   while not done:
-    states, actions, rewards = [], [], []
-    returns = 0
-    epsilon = epsilon-(0.1/100)
-    while True:
-      states.append(observation)
-      action = epsilon_greedy(Q, observation, nA, epsilon)
-      actions.append(action)
-      observation, reward, done, _  = env.step(action)
-      rewards.append(reward)
-
-      if done:
-        break
-
-    for j in range(len(states) - 1, -1, -1):
-      S = states[j]
-      R = rewards[j]
-      A = actions[j]
-      state_action = (S,A)
-      returns = gamma*returns + R
-
-      if state_action not in [(states[k],actions[k]) for k in range(j)]:
-        returns_sum[S,A] += returns
-        returns_count[S,A] += 1
-        Q[S][A] = (returns_sum[S,A])/returns_count[S,A]
-
-
+    action = env.np_random.choice(range(4), 1).item()
     next_state, reward, done, info = env.step(action)
     moves += 1
 
